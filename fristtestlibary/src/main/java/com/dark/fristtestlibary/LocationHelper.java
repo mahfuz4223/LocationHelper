95f0d6f59834b1ef97524523fee371ec276907f0
MAHFUZ AHMED <40047154+mahfuz4223@users.noreply.github.com>
2021-09-14 22:57:55 +0600

    first commit

--- fristtestlibary/src/main/java/com/dark/fristtestlibary/LocationHelper.java
+++ fristtestlibary/src/main/java/com/dark/fristtestlibary/LocationHelper.java
@@ -2,7 +2,7 @@ package com.dark.fristtestlibary;
 
 import android.location.Location;
 
-public class locationhelper {
+public class LocationHelper {
     public static float getDistanceInKM(double startLatitude,double startLongitude,double endLatitude, double endLongitude){
         float[] result = new float[0];
         Location.distanceBetween(startLatitude,startLongitude,endLatitude,endLongitude,result);
